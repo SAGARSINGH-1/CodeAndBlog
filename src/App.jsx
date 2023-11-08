979e205f177a083aabde9d117e36dc3fe9123624
Sagar Singh <sagar.01ko@gmail.com>
2023-11-08 20:17:28 +0530

    Updated file destinations

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import conf from './conf/conf';
-import Layout from './components/layout/Layout';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import React,{ useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+// import {authService} from './appwrite/auth';
+import {login,logout} from './store/authSlice';
 import Home from './components/Home/Home';
 import SignupForm from './registration/Signup';
 import LoginForm from './registration/Login';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='' element={<Layout />}>
-        <Route path='' element={<Home />}/>
-       
-        <Route path='login' element={<LoginForm />}/>
-        <Route path='signup' element={<SignupForm />}/>
-      </Route>
-    )
-  )
+
 
   return (
     <div className="flex justify-center overflow-hidden">
-      <RouterProvider router={router} />
+      <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Home from './components/Home/Home.jsx'
+import LoginForm from './registration/Login.jsx'
+import SignupForm from './registration/Signup.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='' element={<App />}>
+      <Route path='' element={<Home />}/>
+     
+      <Route path='login' element={<LoginForm />}/>
+      <Route path='signup' element={<SignupForm />}/>
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
