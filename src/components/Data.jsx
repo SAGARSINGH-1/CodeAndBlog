39571ab2363d1ce37776636bc9a400b046f702bc
Manendra Verma <manuvma03@gmail.com>
2023-11-09 14:17:31 +0530

    Add Some random blogs

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -51,10 +51,78 @@ export const topBlog = [
 export const latestBlog = [
     {
         id: 1,
-        img: "./Blogs/Politics-2.jpg",
-        title: "Protest On The Streets ",
+        img: "./Blogs/Lifestyle.jpeg",
+        title: "Living Well: Embrace the Lifestyle.",
+        author: "Kao Ren",
+        date: "01 May 23", 
+        category: "Lifestyle",
+        desc: "Discovering the art of living: Lifestyle, fashion, wellness, and culture."
+    },
+
+    {
+        id: 2,
+        img: "./Blogs/Science.jpeg",
+        title: "Science: A Universe of Knowledge.",
         author: "Andrew Malik",
-        date: "05 Jun 24", 
-        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, nam."
+        date: "07 Apr 23", 
+        category: "Science",
+        desc: "Exploring the wonders of the universe: Science, space, and beyond"
+    },
+
+    {
+        id: 3,
+        img: "./Blogs/Travel.jpeg",
+        title: "Wanderlust: Exploring the Globe.",
+        author: "Samantha Rao",
+        date: "17 Mar 23", 
+        category: "Travel",
+        desc: "Exploring new horizons, cultures, and experiences: The world of travel"
+    },
+
+    {
+        id: 4,
+        img: "./Blogs/Health.jpeg",
+        title: "Fitness Journey: Unleash Your Potential Through Exercise",
+        author: "Leo zang",
+        date: "02 Feb 23", 
+        category: "Health",
+        desc: "Fitness, Achieving Your Best through Active Living and Exercise Routines"
+    },
+]
+
+export const popularblog = [
+    {
+        id: 1,
+        title: "Fitness Journey: Unleash Your Potential Through Exercise",
+        author: "Leo zang",
+        date: "07 Aug", 
+    },
+
+    {
+        id: 2,
+        title: "Productivity Hacks: Mastering Time Management",
+        author: "Kin Cho",
+        date: "07 Mar", 
+    },
+
+    {
+        id: 3,
+        title: "The Cosmos Unveiled: A Journey Through Space and Time",
+        author: "Rakesh Singh",
+        date: "04 Jul", 
+    },
+
+    {
+        id: 4,
+        title: "Gaming Galore: Exploring the World of Video Games",
+        author: "Tim Gore",
+        date: "13 Jun", 
+    },
+
+    {
+        id: 5,
+        title: "Hidden Gems: Exploring Off-the-Beaten-Path Destinations",
+        author: "Lisa Brown",
+        date: "17 Apr", 
     },
 ]
--- src/components/Home/Blogs.jsx
+++ src/components/Home/Blogs.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import Category from './Categorybtn'
-import { latestBlog, topBlog } from '../Data'
+import { latestBlog, popularblog, topBlog } from '../Data'
 
 
 export default function Blogs() {
     const [top, setTop] = useState(topBlog);
     const [latest, setLatest] = useState(latestBlog);
+    const [popular, setPopular] = useState(popularblog)
   return (
     <div className='max-w-6xl mx-auto'>
         {/* Top section */}
@@ -45,23 +46,51 @@ export default function Blogs() {
             </div>
       </div>
 
-      <div className='bloglist border-2 border-black h-[80vh] w-[80vw] m-3 flex'>
-                <div className='border-2 border-red-400 h-[50vh] w-[50vw]'>
-                    <div>
-                        <h2>Latest Blog</h2>
+      <div className='bloglist w-[85vw] m-3 flex gap-3 p-3'>
+                <div className=' w-[65vw]'>
+                    <div className='bg-black text-white mb-5'>
+                        <h2 className="font-Montserrat font-semibold p-3 ml-3">Latest Blog</h2>
                     </div>
                     <div>
                         {
                             latest.map((item) =>{
                                 return(
-                                    <div key={item.id} className='blog flex'>
-                                        <div className='img h-[20vh] w-[20vw]'>
+                                    <div key={item.id} className='blog flex bg-slate-100 hover:bg-slate-200 hover:cursor-pointer mb-5 w-[57vw]'>
+                                       <div className='img relative h-[25vh] w-[25vw]'>
                                             <img className='w-full h-full object-cover hover:scale-105 transition duration-300 ' src={item.img} alt="blog" />
+                                            <div className="absolute inset-x-0 bottom-0 bg-opacity-50 text-white p-2">
+                                                <Category val={item.category} />
+                                            </div>
                                         </div>
-                                        <div className='desc border-2 border-indigo-400'>
-                                            <Category val="Politics" />
-                                            <h1 className="text-xl font-semibold">{item.title}</h1>
-                                            <div className='flex gap-10 ml-5 mt-3'>
+                                        <div className='ml-3 p-3 w-[50vw]'>
+                                            <h1 className="text-2xl font-bold">{item.title}</h1>
+                                            <div className='text-base text-gray-400 flex gap-10 mt-3'>
+                                                <p>{item.author}</p>
+                                                <p>{item.date}</p>
+                                            </div>
+                                            <p className='text-lg text-gray-500 mt-1'>{item.desc}</p>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className='w-[30vw]'>
+                    <div className='bg-black text-white mb-5'>
+                        <h2 className="font-Montserrat font-semibold p-3 ml-3">Popular Blog</h2>
+                    </div>
+                    <div>
+                        {
+                            popularblog.map((item)=>{
+                                return(
+                                    <div key={item.id} className='flex gap-3 p-3 mt-5 hover:bg-slate-100 hover:cursor-pointer'>
+                                        <div>
+                                            <h1 className='text-4xl text-gray-400 font-Montserrat mt-2' >Q{item.id}</h1>
+                                        </div>
+                                        <div>
+                                        <h1 className="text-xl font-semibold">{item.title}</h1>
+                                            <div className='text-base text-gray-400 flex gap-10 mt-3'>
                                                 <p>{item.author}</p>
                                                 <p>{item.date}</p>
                                             </div>
@@ -70,14 +99,6 @@ export default function Blogs() {
                                 )
                             })
                         }
-                    </div>
-                </div>
-                <div className='border-2 border-red-400 h-[30vh] w-[30vw]'>
-                    <div>
-                        <h2>Popular Blogs</h2>
-                    </div>
-                    <div>
-
                     </div>
                 </div>
             </div>
