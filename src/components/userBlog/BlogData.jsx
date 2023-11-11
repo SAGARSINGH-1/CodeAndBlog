c8d23ed40ec77d05317489fb11d67369e771a024
Manendra Verma <manuvma03@gmail.com>
2023-11-11 08:16:08 +0530

    Blog page added

---
+++ src/components/userBlog/BlogData.jsx
@@ -0,0 +1,154 @@
+export const Category = [
+    {
+        id: 1,
+        category: "Health",
+        post: 400,
+    },
+
+    {
+        id: 2,
+        category: "Travel",
+        post: 50,
+    },
+
+    {
+        id: 3,
+        category: "Education",
+        post: 70,
+    },
+
+    {
+        id: 4,
+        category: "Technology",
+        post: 200,
+    },
+
+    {
+        id: 5,
+        category: "Entertainment",
+        post: 100,
+    },
+
+    {
+        id: 6,
+        category: "Sports",
+        post: 10,
+    },
+
+    {
+        id: 7,
+        category: "Art and Creativity",
+        post: 40,
+    },
+
+]
+
+export const Posts = [
+    {
+        id: 1,
+        userimg: "../Blogs/User.jpeg",
+        username: "Sho Lang",
+        postimg: "../Blogs/Tech.jpeg",
+        posttext: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem id quos dolorem quibusdam quo beatae atque minus, animi repellendus voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, deleniti?",
+        likes: 10,
+        comment: 5,
+        category: "Technology",
+    },
+
+    {
+        id: 2,
+        userimg: "../Blogs/User.jpeg",
+        username: "Sho Lang",
+        postimg: "../Blogs/Travel2.jpeg",
+        posttext: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem id quos dolorem quibusdam quo beatae atque minus, animi repellendus voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, deleniti?",
+        likes: 40,
+        comment: 15,
+        category: "Travel",
+    }
+] 
+
+export const topBlog = [
+
+    {
+        id: 1,
+        icon: "./Blogs/Politics-2.jpg",
+        title: "Protest On The Streets ",
+        author: "Andrew Malik",
+        date: "05 Jun 24", 
+    },
+
+    {
+        id: 2,
+        icon: "./Blogs/Politics-3.jpg",
+        title: "Union Meeting ON go",
+        author: "Katu Nit",
+        date: "02 Jul 23", 
+    },
+
+]
+
+export const TrendBlogs = [
+    {
+        id: 1,
+        img: "./Blogs/Lifestyle.jpeg",
+        title: "Living Well: Embrace the Lifestyle.",
+        author: "Kao Ren",
+        date: "01 May", 
+    },
+
+    {
+        id: 2,
+        img: "./Blogs/Science.jpeg",
+        title: "Science: A Universe of Knowledge.",
+        author: "Andrew Malik",
+        date: "07 Apr", 
+    },
+
+    {
+        id: 3,
+        img: "./Blogs/Travel.jpeg",
+        title: "Wanderlust: Exploring the Globe.",
+        author: "Samantha Rao",
+        date: "17 Mar", 
+    },
+
+    {
+        id: 4,
+        img: "./Blogs/Health.jpeg",
+        title: "Fitness Journey: Unleash Your Potential",
+        author: "Leo zang",
+        date: "02 Feb", 
+    },
+
+    {
+        id: 5,
+        img: "./Blogs/Lifestyle.jpeg",
+        title: "Living Well: Embrace the Lifestyle.",
+        author: "Kao Ren",
+        date: "01 May", 
+    },
+
+    {
+        id: 6,
+        img: "./Blogs/Science.jpeg",
+        title: "Science: A Universe of Knowledge.",
+        author: "Andrew Malik",
+        date: "07 Apr", 
+    },
+
+    {
+        id: 7,
+        img: "./Blogs/Travel.jpeg",
+        title: "Wanderlust: Exploring the Globe.",
+        author: "Samantha Rao",
+        date: "17 Mar", 
+    },
+
+    {
+        id: 8,
+        img: "./Blogs/Health.jpeg",
+        title: "Fitness Journey: Unleash Your Potential",
+        author: "Leo zang",
+        date: "02 Feb", 
+    },
+]
