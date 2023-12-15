14a86d637a19dbb1a5977dc523bdc279e2af9bb9
Sagar Singh <sagar.01ko@gmail.com>
2023-12-15 23:36:07 +0530

    Updated "Loading" text on all screens

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -9,7 +9,9 @@ const LoadingComponent = ({ isContentLoaded, progress, setProgress }) => {
       ) : (
         <div>
             <LoadingBar color='#666699 ' progress={progress} height={3} onLoaderFinished={() => setProgress(0)} />
-            <p className="text-center text-gray-500 mt-5 absolute top-[40%]">Loading...</p>
+            <div className='flex justify-center items-center h-[70vh]'>
+            <p className="font-bold text-center text-gray-500 top-[40%]">Loading...</p>
+            </div>
         </div>
       
       )}
