a14d3b40f0f1033388e865601de5bca73d6e2be7
Manendra Verma <manuvma03@gmail.com>
2023-12-15 22:07:55 +0530

    fix color of bar

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -8,7 +8,7 @@ const LoadingComponent = ({ isContentLoaded, progress, setProgress }) => {
         ""
       ) : (
         <div>
-            <LoadingBar color='#fe5a1d' progress={progress} height={3} onLoaderFinished={() => setProgress(0)} />
+            <LoadingBar color='#666699 ' progress={progress} height={3} onLoaderFinished={() => setProgress(0)} />
             <p className="text-center text-gray-500 mt-5 absolute top-[40%]">Loading...</p>
         </div>
       
