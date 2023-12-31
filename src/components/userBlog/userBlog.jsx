8c9c6a8536a8737067b40b3d4df96a1f9536ec23
Sagar Singh <sagar.01ko@gmail.com>
2023-12-31 19:24:31 +0530

    Updated

--- src/components/userBlog/userBlog.jsx
+++ src/components/userBlog/userBlog.jsx
@@ -110,8 +110,8 @@ export default function UserBlog() {
                             </div>
 
                             <div className='mr-5'>
-                                <div class="flex items-center justify-center">
-                                    <select class="p-3 border border-gray-300 rounded-lg focus:outline-none">
+                                <div className="flex items-center justify-center">
+                                    <select className="p-3 border border-gray-300 rounded-lg focus:outline-none">
                                         <option value="" disabled selected>Filter Search</option>
                                         <option value="Username">Username</option>
                                         <option value="Blog">Blog</option>
